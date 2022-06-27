6871967c50b9dd82fc55a2747a95bcc607b76217
VuDej <drvu47@gmail.com>
2022-06-27 16:33:24 +0200

    added active storage

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,21 +1,38 @@
 class Api::V1::CarsController < ApplicationController
-  def index
-    @cars = Car.all
 
-    render json: @cars, status: 200
+  def index
+    @response = []
+    @cars = Car.all
+    @serialized_cars = CarSerializer.new(@cars).serializable_hash[:data]
+
+    if @serialized_cars.empty?
+      render json: { error: 'not found', error_message: ['No cars found'] }, status: :not_found
+    else
+      @serialized_cars.each do |car|
+        @response << {
+          id: car[:attributes][:id],
+          carModel: car[:attributes][:car_model],
+          carTransmission: car[:attributes][:transmission],
+          carType: car[:attributes][:car_type],
+          rentPrice: car[:attributes][:price_per_day],
+          carDescription: car[:attributes][:description],
+          carImg: car[:attributes][:image_url]
+        }
+      end
+      puts response
+      render json: { data: @response, message: ['All cars loaded'] }, status: :ok
+    end
   end
 
   def show
     @car = Car.find(params[:id])
-
-    render json: @car, status: 200
+    render json: CarSerializer.new(@car).serializable_hash[:data][:attributes], status: :ok
   end
 
   def create
     @car = current_user.cars.new(car_params)
-
     if @car.save
-      render json: { message: 'Car has been successfully created' }
+      render json: CarSerializer.new(@car).serializable_hash[:data][:attributes]
     else
       render json: @car.errors, status: 422
     end
@@ -33,6 +50,7 @@ class Api::V1::CarsController < ApplicationController
   private
 
   def car_params
-    params.require(:car).permit(:car_model, :price_per_day, :description, :photo, :car_type, :transmission, :user_id)
+    params.require(:car).permit(:car_model, :price_per_day, :description,
+                                :car_type, :transmission, :user_id, :image, :image_url)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
+  has_one_attached :image, dependent: :destroy
   has_many :reservations, dependent: :destroy
   has_many :cars, dependent: :destroy
 
